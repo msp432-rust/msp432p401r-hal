3b7bba2ac7976e1f4c74ad6750298346af06fa7e
Caio Tavares <caio.tavares11@gmail.com>
2021-04-06 19:57:25 -0300

    WIP - Creating eUSCI module

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,10 @@
 #![no_std]
 #![feature(llvm_asm)]
 
-extern crate msp432p401r as pac;
 extern crate embedded_hal as hal;
+extern crate msp432p401r as pac;
+
+pub use nb;
 
 pub mod watchdog;
 pub mod gpio;
@@ -11,5 +13,5 @@ pub mod clock;
 pub mod pcm;
 pub mod flash;
 pub mod timer;
-
-pub use nb;
+pub mod eusci;
+pub mod spi;
---
+++ src/spi.rs
@@ -0,0 +1,4 @@
+use hal::spi::Mode;
+use pac::
+
+
